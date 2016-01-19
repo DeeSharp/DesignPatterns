7015de691905dbc13a960e39cba8e7f2e762c52f
DeeSharp <deepak.radhan@tc-techconsulting.com>
2016-01-19 23:25:44 +0000

    New example added

---
+++ FactoryMethod.Example2/Classes.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethod.Example2
+{
+
+    //example from https://msdn.microsoft.com/en-us/library/ee817667.aspx 
+    public abstract class Computer
+    {
+        public abstract int Mhz { get; }
+    }
+
+    public class ConcreteComputer : Computer
+    {
+        public override int Mhz
+        {
+            get
+            {
+                return 500;
+            }
+
+        }
+    }
+
+    public class BrandXComputer : Computer
+    {
+        public override int Mhz
+        {
+            get
+            {
+                return 1500;
+            }
+        }
+    }
+
+
+    public abstract class ComputerFactory
+    {
+        public abstract Computer GetComputer();
+    }
+
+    public class ConcreteComputerFactory : ComputerFactory
+    {
+        public override Computer GetComputer()
+        {
+            return new ConcreteComputer();
+        }
+    }
+
+    public class BrandXComputerFactory : ComputerFactory
+    {
+        public override Computer GetComputer()
+        {
+            return new BrandXComputer();
+        }
+    }
+
+    public class ComputerAssembler
+    {
+        public void AssembleComputer(ComputerFactory factory)
+        {
+            Computer computer = factory.GetComputer();
+            Console.WriteLine($"assembled a {computer.GetType().Name} running at {computer.Mhz}");
+        }
+    }
+}
